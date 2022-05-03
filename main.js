5ab551c8087efc82b451e208b6fc5d6784399ac9
c-mccormick007 <c.mccormick1295@gmail.com>
2022-05-02 22:40:51 -0500

    add the sketch functionality

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+
+let box = document.getElementsByClassName("box");
+
 for (i=0;i<16*16;i++){
     const div = document.createElement('div');
     div.className = "box";
     document.getElementById("cont").appendChild(div);
+    box[i].addEventListener("mouseenter", function(e){
+        e.target.style.backgroundColor = "black";
+    }, false);
 }
